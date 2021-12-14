65112d58fcd42ef597de79a765b90d61162c9482
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-12-13 19:44:33 -0500

    nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({handleLogout, currentUser}) => {
 
   return (
     <header className="header">
@@ -8,10 +8,19 @@ const Header = () => {
         <h1>Martial Mart</h1>
       </Link>
 
+      {currentUser ? (
+        <nav>
+          <Link to='/products'>For Sale</Link>
+          <Link to='/about'>About</Link>
+        </nav>
+
+      )
+      
+    }
       <nav>
       <Link to='/products'>For Sale</Link>
-      <Link to='/sign-in'>Sign In</Link>
       <Link to='/about'>About</Link>
+      <Link to='/sign-in'>Sign In</Link>
     </nav>
       
   </header>
--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,13 +1,16 @@
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js'
 
-export default function Layout({children}) {
+export default function Layout({children, handleLogout, currentUser}) {
 
 
 
   return (
     <div>
-    <Header />
+      <Header
+        handleLogout={handleLogout}
+        currentUser={currentUser}
+      />
     { children } 
   <Footer />
     </div>
