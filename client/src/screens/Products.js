97f207a512a7523bbb716ad12118a6fcc6a3911f
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-12-15 10:43:26 -0500

    products css

--- client/src/screens/Products.js
+++ client/src/screens/Products.js
@@ -9,15 +9,17 @@ const Products = ({products}) => {
       <Link to='/products/new'>
       <button>Add Product</button>
       </Link>
+      <div className="product">
       {products.map((product) => (
-        <div className="product">
+        // <div className="product">
           <Link to={`products/${product.id}`}>
-            <img src={product.img_url} alt="product"/>
+            <img src={product.img_url} alt="product" className="productImg"/>
             <h4>{product.name}</h4>
             <h4>${product.price}</h4>
           </Link>
-        </div>
+        // </div>
       ))}
+        </div>
     </div>
   )
 }
