1780694271cc2c18dc17f62dc39f2887c1976b1d
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-12-15 17:01:05 -0500

    homepage picture

--- client/src/screens/Homepage.js
+++ client/src/screens/Homepage.js
@@ -1,11 +1,11 @@
-import Picture from '../assets/Homepagepic.png'
+// import Picture from '../assets/Homepagepic.png'
 import './Homepage.css';
 
 const Homepage = () => {
 
   return (
     <div className="homepage">
-      <img src={Picture} alt="Kick"/>
+      <img src="https://cdn.shopify.com/s/files/1/0217/6398/1384/articles/maxresdefault.jpg?v=1562930312" alt="Kick"/>
       <h2>Buy and sell your martial arts gear</h2>
     </div>
   )
